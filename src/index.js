8b2f9807fd95ba7d3a2160ca1833be881c9c3926
Sara Nascimento <53081056+saranascimento9@users.noreply.github.com>
2026-01-30 17:50:55 +0000

    Updated some weather data from the API:
    - Description
    - Feel temperature
    - humidity %
    - wind speed (with the m/s(from API) to km/h conversion)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 // Separation of concerns, function needs to do one thing and do it well
 
 function updatedWeather(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let feelElement = document.querySelector("#feel");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
   let temperature = response.data.temperature.current;
+  let description = response.data.condition.description;
+  let feel = Math.round(response.data.temperature.feels_like);
+  let humidity = response.data.temperature.humidity;
+  let wind = response.data.wind.speed * 3.6;
+
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = description;
+  feelElement.innerHTML = `${feel}ºC`;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${Math.round(wind)} km/h`;
 
   let city = document.querySelector("#city");
   city.innerHTML = response.data.city;
